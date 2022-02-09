10b0fd5f46e3a99dfdd54f8eec01e7b7890c158e
Will Tesler <willtesler@gmail.com>
2022-02-09 02:11:06 -0500

    Update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,7 @@
-/** @type {import('@sveltejs/kit').Config} */
 import vercel from '@sveltejs/adapter-vercel';
+
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: vercel()
 	}
 };
